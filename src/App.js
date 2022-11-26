52c84a8e51fc191597bf14d551485ea7acc6a3c7
ovaisansari <ovaisansari12@gmail.com>
2022-11-26 16:34:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [value, setvalue] = useState(0)
+  let style1 = { color: "blue" };
+  let style2 = { color: "red" };
+  let style3 = { color: "purple" };
+  let style4 = { color: "white" };
 
+    return(
+        <>
+            <div className='main'>
+                <div className='center'>
+                    <h1 
+                      style={
+                        value % 3 === 0 && value % 5 === 0 && value != 0 ? style3 :
+                        value % 3 === 0 && value != 0 ? style1 :
+                        value % 5 === 0 && value != 0 ? style2 :
+                        style4
+                      }> {value} </h1>
+                    <div className='button'>
+                        <button onClick={() => setvalue(value+1)}>
+                          Increment </button>
+                        <button onClick={() => {
+                          if (value > 0) {
+                          setvalue(value-1)}
+                          if(value==0){
+                          alert("Sorry!!!, Zero Limit Reached")
+                          }
+                          }
+                        }>
+                          Decrement </button>
+                        <button onClick={() => setvalue(0)}>
+                          Reset </button>
+                    </div>
+                    <h2 
+                      style={
+                        value % 3 === 0 && value % 5 === 0 && value != 0 ? style3 :
+                        value % 3 === 0 && value != 0 ? style1 :
+                        value % 5 === 0 && value != 0 ? style2 :
+                        style4
+                      }>
+                      {
+                        value % 3 === 0 && value % 5 === 0 && value != 0 ? "Fizz Buzz":
+                        value % 3 === 0 && value != 0 ? "Fizz" :
+                        value % 5 === 0 && value != 0 ? "Buzz" :   
+                        "."                     
+                      }
+                    </h2>
+                </div>
+            </div>
+        </>
+    )
+}
 export default App;
+
